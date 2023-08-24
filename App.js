e310e914c193fe23baee80b2d2cfda44e57bc751
Andrew Macheret <amacheret@atlassian.com>
2023-08-24 13:31:23 -0700

    Get users

--- App.js
+++ App.js
@@ -1,10 +1,48 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+
+const API_BASE_URL =
+  "https://223nwk5397.execute-api.us-west-1.amazonaws.com/dev/api";
+
+const getUsers = async () => {
+  const url = `${API_BASE_URL}/users`;
+  const response = await fetch(url);
+  console.log("@@@@", response);
+  const json = await response.json();
+  console.log("@@@@", json);
+  return json.users;
+};
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
 export default function App() {
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    getUsers().then(setUsers);
+  }, []);
+
+  if (users === null) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!!!!!! WOOOs</Text>
+      <FlatList
+        data={users}
+        renderItem={({ item }) => <Item title={item.name} />}
+        keyExtractor={(item) => item.user_id}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +54,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
 });
