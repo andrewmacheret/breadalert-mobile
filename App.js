853a316659bcb57f3a39b360629a5837bec26494
Johan Johansson <jjohansson82@gmail.com>
2023-09-06 13:11:44 -0700

    added group list

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { useEffect, useState, useCallback } from "react";
+import { StyleSheet, Text, View, FlatList, Pressable } from "react-native";
 
 const API_BASE_URL =
   "https://223nwk5397.execute-api.us-west-1.amazonaws.com/dev/api";
@@ -14,18 +14,38 @@ const getUsers = async () => {
   return json.users;
 };
 
-const Item = ({ title }) => (
-  <View style={styles.item}>
-    <Text style={styles.title}>{title}</Text>
-  </View>
-);
+const getMemberships = async () => {
+  const url = `${API_BASE_URL}/users/123/memberships`;
+  const response = await fetch(url);
+  console.log("@@@@", response);
+  const json = await response.json();
+  console.log("@@@@", json);
+  return json.memberships;
+};
+
+const Item = ({ title, groupId, changeGroup }) => {
+  
+  const log = useCallback(() => {
+    changeGroup(groupId);
+  }, [groupId]);
+
+  return (
+    <Pressable onPress={log}>
+      <View style={styles.item}>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+    </Pressable>
+    );
+}
 
 export default function App() {
   const [users, setUsers] = useState(null);
+  const [groupId, setGroupId] = useState(null);
+  console.log('Hello ', groupId);
 
   useEffect(() => {
-    getUsers().then(setUsers);
-  }, []);
+    getMemberships().then(setUsers);
+  }, [setUsers]);
 
   if (users === null) {
     return (
@@ -34,14 +54,20 @@ export default function App() {
         <StatusBar style="auto" />
       </View>
     );
+  } else if(groupId === null) {
+    return (<GroupList users={users} changeGroup={setGroupId}/>);  
   }
+  return (<Text style={styles.item}>Hello{groupId}</Text>)
+}
+
+function GroupList({users, changeGroup}) {
 
   return (
     <View style={styles.container}>
       <FlatList
         data={users}
-        renderItem={({ item }) => <Item title={item.name} />}
-        keyExtractor={(item) => item.user_id}
+        renderItem={({ item }) => <Item title={item.group_name} groupId={item.group_id} changeGroup={changeGroup}/>}
+        keyExtractor={(item) => item.membership_id}
       />
       <StatusBar style="auto" />
     </View>
@@ -57,6 +83,7 @@ const styles = StyleSheet.create({
   },
   item: {
     backgroundColor: "#f9c2ff",
+    alignItems: "center",
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
