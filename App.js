8da3a291249f9ac6fd034c3c707024e20e6bea2f
Johan Johansson <jjohansson82@gmail.com>
2023-09-07 11:47:59 -0700

    started with ThreadItem, added some questions

--- App.js
+++ App.js
@@ -23,7 +23,16 @@ const getMemberships = async () => {
   return json.memberships;
 };
 
-const Item = ({ title, groupId, changeGroup }) => {
+const getThreads = async (groupId) => {
+  const url = `${API_BASE_URL}/groups/${groupId}/threads`;
+  const response = await fetch(url);
+  console.log("@@@@", response);
+  const json = await response.json();
+  console.log("@@@@", json);
+  return json.threads;
+};
+
+const GroupItem = ({ title, groupId, changeGroup }) => {
   
   const log = useCallback(() => {
     changeGroup(groupId);
@@ -38,11 +47,28 @@ const Item = ({ title, groupId, changeGroup }) => {
     );
 }
 
+const ThreadItem = ({ title, threadId, changeThread }) => {
+  
+  const log = useCallback(() => {
+    changeThread(threadId);
+  }, [threadId]);
+
+  return (
+    <Pressable onPress={log}>
+      <View style={styles.item}>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+    </Pressable>
+    );
+}
+
 export default function App() {
   const [users, setUsers] = useState(null);
   const [groupId, setGroupId] = useState(null);
+  const [threadId, setThreadId] = useState(null);
   console.log('Hello ', groupId);
 
+  // move???
   useEffect(() => {
     getMemberships().then(setUsers);
   }, [setUsers]);
@@ -55,18 +81,40 @@ export default function App() {
       </View>
     );
   } else if(groupId === null) {
-    return (<GroupList users={users} changeGroup={setGroupId}/>);  
+    return (<GroupList users={users} changeGroup={setGroupId}/>);
   }
-  return (<Text style={styles.item}>Hello{groupId}</Text>)
+
+  // then what?
+  useEffect(() => {
+    getThreads(groupId).then(setUsers);
+  }, [setUsers]);
+
+  return (<ThreadList users={users} changeThread={setThreadId}/>);
 }
 
+// TODO: rename users
+function ThreadList({users, changeThread}) {
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={users}
+        renderItem={({ item }) => <ThreadItem title={item.title} threadId={item.thread_id} changeThread={changeThread}/>}
+        keyExtractor={(item) => item.thread_id}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+// TODO: rename users
 function GroupList({users, changeGroup}) {
 
   return (
     <View style={styles.container}>
       <FlatList
         data={users}
-        renderItem={({ item }) => <Item title={item.group_name} groupId={item.group_id} changeGroup={changeGroup}/>}
+        renderItem={({ item }) => <GroupItem title={item.group_name} groupId={item.group_id} changeGroup={changeGroup}/>}
         keyExtractor={(item) => item.membership_id}
       />
       <StatusBar style="auto" />
